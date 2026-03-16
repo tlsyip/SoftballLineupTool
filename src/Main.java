98ea882c03a3b79f7e6fa612921cc20759f94984
tlsyip <tlsyip@gmail.com>
2026-03-15 21:55:20 -0400

    printing inning 1 lineups

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,30 @@ import java.util.Scanner;
 
 public class Main {
     private static ArrayList<Player> battingLineup = new ArrayList<>();
+    private static ArrayList<String> fieldingLineup = new ArrayList<>();
     private static int currentBatterIndex = 0;
     private static int inning = 1;
+    private static boolean batting;
     private static Scanner input = new Scanner(System.in);
+
 
     public static void main(String[] args) {
         loadBattingLineup();
+        loadFieldingLineup();
+        System.out.println("Enter side: Home/Away");
+        String side = (input.nextLine());
+        if (side.equalsIgnoreCase("Home")) {
+            batting = false;
+            printFieldingLineup();
+        }
+        else if (side.equalsIgnoreCase("Away")) {
+            batting = true;
+            printBattingOrder();
+        }
+        else {
+            System.out.println("Invalid input. Quitting program.");
+            System.exit(0);
+        }
     }
 
     private static void loadBattingLineup () {
@@ -22,9 +40,63 @@ public class Main {
                 }
             }
             fileScanner.close();
-            System.out.println("Batting lineup loaded: " + battingLineup.size() + " players.");
+            System.out.println("Batting lineup loaded: " + battingLineup.size() + " players found.");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
+
+    private static void loadFieldingLineup() {
+        try {
+            Scanner fileScanner = new Scanner(new File("FieldingLineup.txt"));
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine().trim();
+                if (!line.isEmpty()) {
+                    fieldingLineup.add(line);
+                }
+            }
+            fileScanner.close();
+            System.out.println("Fielding lineup loaded: " + fieldingLineup.size() + " innings found.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void printBattingOrder() {
+        System.out.println("\nCurrent Batting Order:");
+        for (int i = 0; i < battingLineup.size(); i++) {
+            String marker = "<- Next up";
+            if (i==currentBatterIndex) {
+                System.out.println((i + 1) + ". " + battingLineup.get(i) + marker);
+            }
+            else {
+                System.out.println((i + 1) + ". " + battingLineup.get(i));
+            }
+        }
+    }
+
+    private static void printFieldingLineup() {
+        System.out.println("\nFielding Lineup for Inning " + inning + ":\n");
+        if (inning - 1 < fieldingLineup.size()) {
+            String line = fieldingLineup.get(inning - 1);
+            String[] positions = line.split(",");
+            for (String pos : positions) {
+                System.out.println(pos.trim());
+            }
+        } else {
+            System.out.println("No fielding lineup for this inning.");
+        }
+    }
+
+    private static void nextHalfInning() {
+        if (batting) {
+            batting = false;
+            printFieldingLineup();
+        }
+        else {
+            printBattingOrder();
+        }
+    }
+
+
 }
